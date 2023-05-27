1a841ea00a6d3989f8106c46ebd08b5c398f621e
Maiz <me@maizify.com>
2023-05-27 15:10:52 +0800

    feat: change to custom config

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://maizify.com/media/site/avatar-02.jpg',
+      name: 'Maiz',
+      description: '<a href="javascript:void(0)" onclick="(function() { location.reload() })()" class="text-blue-500">Refresh</a>',
     },
   }
 }
